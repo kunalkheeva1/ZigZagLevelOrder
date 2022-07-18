d83869520b09ba2862b48b785572a3882c88d27f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-17 20:59:52 -0700

    added the comments for better understanding

--- src/ZigzagLevelOrder.java
+++ src/ZigzagLevelOrder.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
 public class ZigzagLevelOrder {
+
+    // get a node that stores val, and left and right
     class TreeNode{
         int val;
         TreeNode left;
@@ -11,24 +13,34 @@ public class ZigzagLevelOrder {
             this.left = left;
         }
     }
+    // method to return a list of lists
     public List<List<Integer>> zigZaglevelOrder(TreeNode root){
+        //get a list of list
        List<List<Integer>> ans = new ArrayList<>();
+       //if root is null then simply return the list
         if(root == null) return ans;
-
+        //queue will contain the roots by levels
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         int level =1;
-        while(!q.isEmpty()){
+        // until the queue is empty
+        while(!q.isEmpty())
+        {
+            // get a list to store the datas of the nodes
             int size = q.size();
             ArrayList<Integer> list = new ArrayList<>();
 
-            while(size> 0){
+            while(size> 0)
+            {   // remove the node from the queue and add its value to the list
+                // and decrease the size
                 TreeNode current = q.remove();
                 list.add(current.val);
                 size--;
+                // check the left and right too
                 if(current.left != null) q.add(current.left);
                 if(current.right != null) q.add(current.right);
             }
+            // if level is odd then simply add it, otherwise reverse it first
             if(level %2 == 1)
             {
                 ans.add(list);
@@ -37,9 +49,10 @@ public class ZigzagLevelOrder {
             {
                 Collections.reverse(list);
                 ans.add(list);
-            }
+            }// also increment the level after every traversal
                 level ++;
-        }return ans;
+        } // return the ans
+        return ans;
 
 
 
