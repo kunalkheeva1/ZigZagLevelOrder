0e9b985871da340681c54976a103b6ddce1a5bf0
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-17 20:35:39 -0700

    created a treenode class

--- src/ZigzagLevelOrder.java
+++ src/ZigzagLevelOrder.java
@@ -1,5 +1,15 @@
 public class ZigzagLevelOrder {
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.right = right;
+            this.left = left;
+        }
+    }
     public static void main(String[] args) {
-        
+
     }
 }
