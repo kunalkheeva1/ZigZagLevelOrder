0b6f26c8debc129adb929825eba99de3d077c8f5
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-17 20:55:42 -0700

    created a method to return a zigzag level order traversal

--- src/ZigzagLevelOrder.java
+++ src/ZigzagLevelOrder.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class ZigzagLevelOrder {
     class TreeNode{
         int val;
@@ -9,6 +11,40 @@ public class ZigzagLevelOrder {
             this.left = left;
         }
     }
+    public List<List<Integer>> zigZaglevelOrder(TreeNode root){
+       List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int level =1;
+        while(!q.isEmpty()){
+            int size = q.size();
+            ArrayList<Integer> list = new ArrayList<>();
+
+            while(size> 0){
+                TreeNode current = q.remove();
+                list.add(current.val);
+                size--;
+                if(current.left != null) q.add(current.left);
+                if(current.right != null) q.add(current.right);
+            }
+            if(level %2 == 1)
+            {
+                ans.add(list);
+            }
+            else
+            {
+                Collections.reverse(list);
+                ans.add(list);
+            }
+                level ++;
+        }return ans;
+
+
+
+    }
+
     public static void main(String[] args) {
 
     }
